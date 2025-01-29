e2a48881cf8773aed797422f6c32257c98d7bcf9
CatRespect <alexej.feofanov@mail.ru>
2025-01-29 11:08:38 +0300

    writing hash function

---
+++ src/main/java/paka/tinder/tinder/Secure/Нa$h.java
@@ -0,0 +1,55 @@
+package paka.tinder.tinder.Secure;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Arrays;
+import java.util.Base64;
+
+public class Нa$h {//If everything will blow UP, pick up your items, documents and run into a forest
+//(or delete first russian letter in name)
+    private static final SecureRandom Rand = new SecureRandom();
+    public static final  int ITERATIONS = 1000-7;
+    public static final  int KEY_LENGTH = 512;
+    private static final String ALGORITHM = "PBKDF2WithHmacSHA512";
+
+    public static String genSalt(int length){
+        byte[] salt =  new byte[length];
+        Rand.nextBytes(salt);
+
+        StringBuilder cursed_salt = new StringBuilder();
+        for (byte b : salt) {
+            cursed_salt.append((char) (b + 256));
+        }
+        return cursed_salt.toString();
+    }
+    public String HaShIt(String password, String cursed_salt){
+        char[] pass = password.toCharArray();
+        byte[] salt = new byte[cursed_salt.length()];
+        for (int i=0; i<cursed_salt.length();++i) {
+            salt[i]=(byte)(cursed_salt.charAt(i)-256);
+        }
+
+        PBEKeySpec spec = new PBEKeySpec(pass, salt, ITERATIONS, KEY_LENGTH);
+
+        Arrays.fill(pass,Character.MIN_VALUE);
+        String retur;
+        try {
+            SecretKeyFactory fac = SecretKeyFactory.getInstance(ALGORITHM);
+            byte[] securePassword = fac.generateSecret(spec).getEncoded();
+            StringBuilder result = new StringBuilder();
+            for (byte b : salt) {
+                result.append((char) (b + 256));
+            }
+            retur = result.toString();
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException e) {
+            System.out.println(e);
+            System.out.println(e.getStackTrace());
+            retur = "";
+        } finally {
+            spec.clearPassword();
+        }
+        return retur;
+    }
+}
--- src/main/java/paka/tinder/tinder/TinderApplication.java
+++ src/main/java/paka/tinder/tinder/TinderApplication.java
@@ -3,6 +3,7 @@ package paka.tinder.tinder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import paka.tinder.tinder.Secure.BillCipher;
+import paka.tinder.tinder.Secure.Нa$h;
 
 import java.security.PublicKey;
 
@@ -13,8 +14,13 @@ import java.security.PublicKey;
 public class TinderApplication {
 
     public static void main(String[] args) {
+        int length = 28;
+        String salt = Нa$h.genSalt(length);
+        System.out.println("salt = " + salt);
+        salt = Нa$h.genSalt(length);
+
         //Turn on server
-        SpringApplication.run(TinderApplication.class, args);
+//        SpringApplication.run(TinderApplication.class, args);
 
     }
 
